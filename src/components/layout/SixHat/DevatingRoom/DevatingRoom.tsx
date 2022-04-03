d09305851c72c5247ba3b74ee6462df5bcfa6828
yechan <noy3928@naver.com>
2022-04-03 16:27:10 +0900

    feat : make complete button in debating page

--- src/components/layout/SixHat/DevatingRoom/DevatingRoom.tsx
+++ src/components/layout/SixHat/DevatingRoom/DevatingRoom.tsx
@@ -1,18 +1,30 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import { CenterLayout } from '../../../common';
+import { CenterLayout, PrimaryButton } from '../../../common';
 import { DevatingChatBox } from '../DevatingChatBox';
+import { useAppSelector } from '@redux/hooks';
+import { selectSixHat } from '@redux/modules/sixHat';
 
 type DevatingRoom = {
   onClick: (arg: string) => void;
 };
 
 const DevatingRoom = ({ onClick }: DevatingRoom) => {
+  const { isAdmin } = useAppSelector(selectSixHat);
+
   return (
     <CenterLayout>
-      <DevatingChatBox onClick={onClick} />
+      <>
+        <DevatingChatBox onClick={onClick} />
+        <Empty />
+        {isAdmin && <PrimaryButton text="완료" height={50} width={150} />}
+      </>
     </CenterLayout>
   );
 };
+
+const Empty = styled.div`
+  height: 10px;
+`;
 
 export { DevatingRoom };
