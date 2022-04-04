87ab2a7340293c1651453365e69fd645dd199536
dongyun <han10091009@naver.com>
2022-04-04 13:40:55 +0900

    css: layout>BrainWriting

---
+++ src/components/layout/BrainWriting/VotingRoom/VoteCardModal/VoteCardModal.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { VoteModal } from '@components/common/Modals';
+import { Button } from '@components/common';
+import styled from 'styled-components';
+import { themedPalette } from '../../../../../theme/styleTheme';
+import { useRouter } from 'next/router';
+import { DetailCard } from '../DetailCard';
+import CloseIcon from '@mui/icons-material/Close';
+
+interface propsType {
+  closeModal: () => void;
+}
+const VoteCardModal = (props: propsType) => {
+  const router = useRouter();
+  //   console.log(props.closeModal);
+
+  const moveToMain = () => {
+    router.push('/');
+  };
+
+  return (
+    <VoteModal>
+      <DetailCard width={400} height={630}>
+        sdsss
+      </DetailCard>
+      <CloseIcon
+        onClick={props.closeModal}
+        sx={{ position: 'absolute', top: 30, left: 340, cursor: 'pointer' }}
+      />
+    </VoteModal>
+  );
+};
+
+export { VoteCardModal };
+
+/**
+ * TODO : 1.모든 요소 입력 안되었을 때, 버튼 diabled 처리하기
+ */
